da89e01de1df921f1880b5d18a3c8392227beed8
icaroinflames <rczurita@gmail.com>
2022-01-26 18:52:56 +0000

    api in docker

--- multi/features/multiply/multiply-route/index.js
+++ multi/features/multiply/multiply-route/index.js
@@ -1,6 +1,17 @@
 const multiplyRoute = (req, reply) => {
     console.log(req.body);
 
+    if(req.body.input.num1 === 5 || req.body.input.num2 === 5){
+        const message = "we dont like num 5";
+        const extension = {
+            path: "$.input.num1",
+            code: 23
+        }
+        reply.status(418).send({
+            message,
+            extension
+        });
+    } 
     const result = req.body.input.num1 * req.body.input.num2;
     reply.send({result});
 }
